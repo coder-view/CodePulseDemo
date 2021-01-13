3125dc9cfbaee51e3398ab5e3b2272d67394286a
kuroky <kuroky@DESKTOP-5OQ2588>
2021-01-13 11:18:26 +0800

    add

---
+++ codepulse/src/test/java/com/codepulse/test/CodepulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepulse.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodepulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

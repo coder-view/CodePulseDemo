0a9794de8ef94f7adacaabc33c9b31394fca1d85
kuroky <kuroky@DESKTOP-5OQ2588>
2021-01-15 18:01:14 +0800

    http://localhost:9000/cp/test/getdate

---
+++ cp/com/codepulse/test/CodepulseApplication.java
@@ -0,0 +1,13 @@
+package com.codepulse.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodepulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodepulseApplication.class, args);
+	}
+
+}
